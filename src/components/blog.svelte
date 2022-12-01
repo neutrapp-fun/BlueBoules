4cf143e690ada2f39a1a628ae1378dfb893fef71
Hugo <picard.hugo08@gmail.com>
2022-12-01 23:28:01 +0100

    blog ok; need design

---
+++ src/components/blog.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let blog;
+    console.log(blog)
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    let tag = blog.tags.split(";").join(' | ')
+</script>
+
+<div class="m-4">
+    <div class="flex">
+        <img class="rounded-full mr-4" src="https://picsum.photos/50/50?{getRandomInt(200)}" alt="">
+        <h2 class="text-lg text-gray-700 flex items-center">Anonym{getRandomInt(2000)} <span
+                class="text-sm text-gray-600">  - {tag}</span></h2>
+    </div>
+    <div class="flex flex-col align-left">
+        <h2 class="ml-16 text-left">{blog.content}</h2>
+        <h4 class="ml-16 text-left text-xs mt-2">{blog.date.split("T")[1]} - {blog.date.split("T")[0]}</h4>
+
+    </div>
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
-<a href="/branch/2"> Click</a>
+<script>
+    import {blog} from '../store/blog_store.js'
+    import Blog from '../components/blog.svelte'
+</script>
+<div class="flex bg-grey-500 h-full">
+    <div class="flex-auto w-96 row">
+        <h1 class="text-3xl w-full">Blue Boules<span class="text-9xl text-blue-600">..</span></h1>
+    </div>
+    <div class="flex-auto w-full text-center">
+        {#each $blog as article}
+                <Blog blog={article}></Blog>
+            {/each}
+    </div>
+    <div class="flex-auto w-64 text-center">
+        03
+    </div>
+</div>
