4c56e6a7f49d9f73c11d814afa503a5f33b1de33
Dyspromium <remy.duclos@hotmail.fr>
2022-12-02 04:34:34 +0100

    Filter Manage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,119 +1,163 @@
 <script>
-    import {blog} from '../store/blog_store.js'
-    import Post from '../components/post.svelte'
+	import { blog } from '../store/blog_store.js';
+	import Post from '../components/post.svelte';
 
-    import {Icon, ChevronDown, Play} from "svelte-hero-icons";
+	import { Icon, Play } from 'svelte-hero-icons';
 
-    let filters = []
+	import supabase from '../lib/supabase';
 
-  let checkbox;
+	async function getTags() {
+		const { data, error } = await supabase.from('tags').select();
+
+		tags = data;
+	}
+
+    // async function getBlogs(){
+    //     const { data, error } = await supabase.rpc('get_post_with_limit', {
+    //         num,
+    //         post_date
+    //     }).then(({data, error}) => {
+    //         if (error) {
+    //             console.log('error to fetch posts : ', error)
+    //             return;
+    //         }
+
+    //     blog.set(data);
+    // });
+
+	// 	blog = data;
+    // }
+
+
+
+	let tags = [];
+	let tagSelected = 'FILTRE';
+
+	getTags();
+
+	let filters = [];
+	function addTag() {
+        if(tagSelected != "FILTRE")
+		    filters = [...filters, tagSelected].filter((val, index, newlist)=> newlist.indexOf(val) == index)
+	}
+
+    function removeTag(tag){
+        filters = filters.filter(function(value, index, arr){ 
+        return value != tag;
+    })
+    }
+
+	let checkbox;
 	let captcha = false;
-  let disabled = true;
+	let disabled = true;
 
-  function handleClick(event) {
-    disabled = !checkbox.checked;
-
-  }
-
+	function handleClick(event) {
+		disabled = !checkbox.checked;
+	}
 </script>
+
 <div class="flex bg-gray-50 h-screen text-gray-600">
-    <div class="p-8 md:flex flex-col h-full justify-between hidden">
-        <img src="logo.png" alt=""/>
-        
-        <form class="w-full max-w-sm p-8">
-          <div class="text-center items-center justify-center ">
-            <h1 class="text-xl text-[#5A5A5A] mb-6">Poster, pas de Taboo, Safe place</h1>
-          </div>
-          <div class="flex justify-center items-center mb-6">
-            <div class="w-full">
-              <input
-                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
-                id="inline-full-name"
-                type="text"
-                placeholder="Tags"
-                required
-              />
-            </div>
-          </div>
-          <div class="flex justify-center mb-6">
-            <div class="w-full">
-              <textarea
-                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
-                id="story"
-                name="story"
-                rows="5"
-                cols="33"
-                required
-              />
-            </div>
-          </div>
-          <div class=" mb-6">
-            <div class="" />
-            <label class=" block text-gray-500 font-bold">
-              <input
+	<div class="p-8 md:flex flex-col h-full justify-between hidden">
+		<img src="logo.png" alt="" />
 
-                class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-bl"
-                type="checkbox"
-                bind:this={checkbox}
-                on:click={handleClick}
+		<form class="w-full max-w-sm p-8">
+			<div class="text-center items-center justify-center ">
+				<h1 class="text-xl text-[#5A5A5A] mb-6">Poster, pas de Taboo, Safe place</h1>
+			</div>
+			<div class="flex justify-center items-center mb-6">
+				<div class="w-full">
+					<input
+						class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
+						id="inline-full-name"
+						type="text"
+						placeholder="Tags"
+						required
+					/>
+				</div>
+			</div>
+			<div class="flex justify-center mb-6">
+				<div class="w-full">
+					<textarea
+						class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
+						id="story"
+						name="story"
+						rows="5"
+						cols="33"
+						required
+					/>
+				</div>
+			</div>
+			<div class=" mb-6">
+				<div class="" />
+				<label class=" block text-gray-500 font-bold">
+					<input
+						class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-bl"
+						type="checkbox"
+						bind:this={checkbox}
+						on:click={handleClick}
+					/>
+					<span class="text-sm"> je ne suis pas un robot </span>
+				</label>
+			</div>
 
-              />
-              <span class="text-sm"> je ne suis pas un robot </span>
-            </label>
-          </div>
+			<div class="w-full group block buttons">
+				<button
+					{disabled}
+					class="absolute shadow transition bg-white hover:bg-[#6A99C4] focus:shadow-outline focus:outline-none text-slate-900 hover:text-white font-bold py-3 px-20 rounded "
+					type="submit"
+				>
+					Envoyer
+				</button>
+			</div>
+		</form>
 
-          <div class="w-full group block buttons">
-            <button {disabled} 
-              class="absolute shadow transition bg-white hover:bg-[#6A99C4] focus:shadow-outline focus:outline-none text-slate-900 hover:text-white font-bold py-3 px-20 rounded "
-              type="submit"
-            >
-              Envoyer
-            </button>
-          </div>
-        </form>
+		<a href="/game">
+			<div
+				class="bg-black w-full h-48 flex flex-col items-center justify-center rounded-none hover:rounded-xl transition-all duration-300"
+			>
+				<p class="text-center text-white font-bold text-xl">Blue Boules <br />The Game</p>
+				<Icon src={Play} solid size="30" class="mt-4 text-white" />
+			</div>
+		</a>
+	</div>
+	<div class="grow bg-white p-4 md:p-10">
+		<div class="flex justify-between mb-4">
+			<img src="logo.png" alt="" class="h-6 md:hidden flex" />
+			<div class="flex items-center space-x-2">
+				{#each filters as a}
+                    <button on:click={removeTag(a)}>
+                        <div class="p-1 rounded-xl border border-gray-100">{a}</div>
+                    </button>
+				{/each} 
+			</div>
+			<select
+				bind:value={tagSelected}
+				on:change={addTag}
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2"
+			>
+				<option selected>FILTRE</option>
+				{#each tags as tag}
+                
+					<option value={tag.name}>{tag.name}</option>
+				{/each}
+			</select>
+		</div>
 
-        <a href="/game">
-            <div class="bg-black w-full h-48 flex flex-col items-center justify-center rounded-none hover:rounded-xl transition-all duration-300">
-                <p class="text-center text-white font-bold text-xl">Blue Boules <br>The Game</p>
-                <Icon src={Play} solid size="30" class="mt-4 text-white"/>
-
-            </div>
-        </a>
-    </div>
-    <div class="grow bg-white p-4 md:p-10">
-
-        <div class="flex justify-between mb-4">
-            <img src="logo.png" alt="" class="h-6 md:hidden flex"/>
-            <div>
-                {#each filters as tag}
-                    <div>{tag}</div>
-                {/each}
-            </div>
-            <button class="flex items-center justify-end hover:bg-gray-100 px-2 transition-all duration-300 rounded-xl">
-                Filter 
-                <Icon src={ChevronDown} solid size="30" class="ml-2 text-primary"/>
-            </button>
-
-        </div>
-
-        {#each $blog as post}
-            <Post {post}></Post>
-        {/each}
-    </div>
-    <div class="w-52 p-8 hidden md:block">
-        <h2>Tendances Top BBL </h2>
-    </div>
+		{#each $blog as post}
+			<Post {post} />
+		{/each}
+	</div>
+	<div class="w-52 p-8 hidden md:block">
+		<h2>Tendances Top BBL</h2>
+	</div>
 </div>
 
 <style>
-  
 	button:disabled {
-    background-color: gray;
+		background-color: gray;
 	}
 
-  .buttons:hover button:disabled {
-    @apply translate-x-64;
-  }
-
-
+	.buttons:hover button:disabled {
+		@apply translate-x-64;
+	}
 </style>
