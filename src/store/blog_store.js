c8e923a9e51dae07756a9e10516caa5ef0cabe78
Hugo <picard.hugo08@gmail.com>
2022-12-02 06:31:52 +0100

    fix: yassine

--- src/store/blog_store.js
+++ src/store/blog_store.js
@@ -16,12 +16,10 @@ export const fetchPosts = (num = 5, post_date = '2022-12-01 17:26:06') => {
         blog.set(data);
     });
 }
+
 export async function upVote(post_id) {
-    console.log(post_id)
     let {data, error} = await supabase
-        .rpc('update_upvotes', {
-            post_id
-        })
+        .rpc('update_upvotes', { post_id })
     if (error) {
         console.log('up vote failed ', error);
         return;
@@ -29,12 +27,7 @@ export async function upVote(post_id) {
     return data.new.upvotes
 }
 
-supabase
-    .channel('supabase_realtime')
-    .on('postgres_changes', {event: '*', schema: 'public'}, (_) => {
-        fetchPosts();
-    })
-    .subscribe();
+supabase.channel('supabase_realtime').on('postgres_changes', {event: '*', schema: 'public'}, () => {fetchPosts();}).subscribe();
 
 
 fetchPosts();
