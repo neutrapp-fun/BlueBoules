9eb53109fae28426cf34b176f02d675f5dbc5392
Hugo <picard.hugo08@gmail.com>
2022-12-02 05:19:48 +0100

    hugo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,12 @@
 
     let filters = []
 
-  let checkbox;
-	let captcha = false;
-  let disabled = true;
-
-  function handleClick(event) {
-    disabled = !checkbox.checked;
-
-  }
-
+    let checkbox;
+    let captcha = false;
+    let disabled = true;
+    function handleClick(event) {
+        disabled = !checkbox.checked;
+    }
 </script>
 <div class="flex bg-gray-50  text-gray-600">
     <div class="p-8 md:flex h-full flex-col justify-between hidden">
@@ -38,68 +35,62 @@
                 </a>
                 <a class="m-2" href="">Contact</a>
             </div>
+            <form class="w-full bg-white p-16 rounded-md ">
+                <div class="text-center items-center justify-center ">
+                    <h1 class="font-bold text-2xl text-[#5A5A5A] mb-6">Ecrivez, pas de Taboo, Safe place</h1>
+                </div>
+                <div class="flex justify-center items-center mb-6">
+                    <div class="w-full">
+                        <input
+                                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
+                                id="inline-full-name"
+                                type="text"
+                                placeholder="Tags"
+                                required
+                        />
+                    </div>
+                </div>
+                <div class="flex justify-center mb-6">
+                    <div class="w-full">
+              <textarea
+                      class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
+                      id="story"
+                      name="story"
+                      rows="5"
+                      cols="33"
+                      placeholder="Je ne sais pas comment faire plaisir à mon partenaire....."
+                      required
+              />
+                    </div>
+                </div>
+                <div class=" mb-6">
+                    <div class="" />
+                    <label class=" block text-gray-500 font-bold">
+                        <input
+
+                                class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-bl"
+                                type="checkbox"
+                                bind:this={checkbox}
+                                on:click={handleClick}
+
+                        />
+                        <span class="text-sm"> je ne suis pas un robot </span>
+                    </label>
+                </div>
+
+                <div class="w-full group block buttons">
+                    <button {disabled}
+                            class="absolute shadow transition bg-gray-100 hover:bg-[#6A99C4] focus:shadow-outline focus:outline-none text-slate-900 hover:text-white font-bold py-3 px-20 rounded "
+                            type="submit"
+                    >
+                        Envoyer
+                    </button>
+                </div>
+            </form>
+
         </div>
 
         <a href="/game" class="mt-32">
-
-<div class="flex bg-gray-50 h-screen text-gray-600">
-    <div class="p-8 md:flex flex-col h-full justify-between hidden">
-        <img src="logo.png" alt=""/>
-        
-        <form class="w-full bg-white p-16 rounded-md ">
-          <div class="text-center items-center justify-center ">
-            <h1 class="font-bold text-2xl text-[#5A5A5A] mb-6">Ecrivez, pas de Taboo, Safe place</h1>
-          </div>
-          <div class="flex justify-center items-center mb-6">
-            <div class="w-full">
-              <input
-                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
-                id="inline-full-name"
-                type="text"
-                placeholder="Tags"
-                required
-              />
-            </div>
-          </div>
-          <div class="flex justify-center mb-6">
-            <div class="w-full">
-              <textarea
-                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6A99C4]"
-                id="story"
-                name="story"
-                rows="5"
-                cols="33"
-                placeholder="Je ne sais pas comment faire plaisir à mon partenaire....."
-                required
-              />
-            </div>
-          </div>
-          <div class=" mb-6">
-            <div class="" />
-            <label class=" block text-gray-500 font-bold">
-              <input
-
-                class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-bl"
-                type="checkbox"
-                bind:this={checkbox}
-                on:click={handleClick}
-
-              />
-              <span class="text-sm"> je ne suis pas un robot </span>
-            </label>
-          </div>
-
-          <div class="w-full group block buttons">
-            <button {disabled} 
-              class="absolute shadow transition bg-gray-100 hover:bg-[#6A99C4] focus:shadow-outline focus:outline-none text-slate-900 hover:text-white font-bold py-3 px-20 rounded "
-              type="submit"
-            >
-              Envoyer
-            </button>
-          </div>
-        </form>
-
-        <a href="/game">
             <div class="bg-black w-full h-48 flex flex-col items-center justify-center rounded-none hover:rounded-xl transition-all duration-300">
                 <p class="text-center text-white font-bold text-xl">Blue Boules <br>The Game</p>
                 <Icon src={Play} solid size="30" class="mt-4 text-white"/>
@@ -120,8 +111,6 @@
         </div>
         <div class="flex justify-between flex-col md:flex-row">
 
-        <div class="flex justify-between mb-4">
-            <img src="logo.png" alt="" class="h-6 md:hidden flex"/>
             <div>
                 {#each filters as tag}
                     <div>{tag}</div>
@@ -131,7 +120,7 @@
                 Blog - Retrouvez ici tout les posts anonymes
             </div>
             <button class="flex items-center justify-end hover:bg-gray-100 px-2 transition-all duration-300 rounded-xl">
-                Filter 
+                Filter
                 <Icon src={ChevronDown} solid size="30" class="ml-2 text-primary"/>
             </button>
 
@@ -162,16 +151,3 @@
         </div>
     </div>
 </div>
-
-<style>
-  
-	button:disabled {
-    background-color: gray;
-	}
-
-  .buttons:hover button:disabled {
-    @apply translate-x-64;
-  }
-
-
-</style>
